48ea45a4024a304298a43c0fec93d79c66d1c7d5
19Phin <19phin@gmail.com>
2025-01-22 20:35:59 -0700

    backprop

--- common/src/main/java/net/dialingspoon/speedcap/SpeedCap.java
+++ common/src/main/java/net/dialingspoon/speedcap/SpeedCap.java
@@ -1,14 +1,6 @@
 package net.dialingspoon.speedcap;
 
 public final class SpeedCap {
-    // dont need all versions allowed
-    // readme
-    // slider up 10* faster not 10* as much
-    // source // /
-    // mixin 21
-    // 1.20.3
-    // pack format
-    // version
     public static final String MOD_ID = "speedcap";
     public static void init() {
     }
