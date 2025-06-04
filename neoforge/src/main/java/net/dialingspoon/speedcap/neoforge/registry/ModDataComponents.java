4132124ee84d76d30a5c05fbcf3a0287ff9fb8c5
DiallingSpoon527 <123208557+19Phin@users.noreply.github.com>
2025-06-04 10:52:47 -0700

    1.21 bugfix

--- neoforge/src/main/java/net/dialingspoon/speedcap/neoforge/registry/ModDataComponents.java
+++ neoforge/src/main/java/net/dialingspoon/speedcap/neoforge/registry/ModDataComponents.java
@@ -3,7 +3,6 @@ package net.dialingspoon.speedcap.neoforge.registry;
 import net.dialingspoon.speedcap.SpeedCap;
 import net.dialingspoon.speedcap.item.CapSettingsComponent;
 import net.minecraft.core.component.DataComponentType;
-import net.minecraft.core.registries.Registries;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -11,7 +10,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.function.UnaryOperator;
 
 public class ModDataComponents {
-    public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, SpeedCap.MOD_ID);
+    public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(SpeedCap.MOD_ID);
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<CapSettingsComponent>> SPEEDCAP_DATA = register("speed_cap", builder -> builder.persistent(CapSettingsComponent.CODEC).networkSynchronized(CapSettingsComponent.STREAM_CODEC));
 
     private static <T>DeferredHolder<DataComponentType<?>, DataComponentType<T>> register(String name, UnaryOperator<DataComponentType.Builder<T>> builderOperator) {
